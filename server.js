1375c0a7a70aafaff9b8939e78742214ddd55874
BintangRF <bintangrama777@gmail.com>
2023-10-08 13:49:53 +0700

    tambahan

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.use(
     secret: "your-secret-key",
     resave: false,
     saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // Sesuaikan dengan kebutuhan Anda
+    },
   })
 );
 
